505840e930b510ca163b90bbc0a0de2f865305c8
Eugene <eugene.matatov@gmail.com>
2022-02-10 16:29:45 +0200

    create user problem

---
+++ .history/backend/data/users_20220207052550.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Tester1",
+    email: "tester@gmail.com",
+    password: bcrypt.hashSync("tester", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Alex",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
---
+++ .history/backend/data/users_20220207052655.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin Usera",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("admin", 10),
+    isAdmin: true,
+  },
+  {
+    name: "tester",
+    email: "tester@gmail.com",
+    password: bcrypt.hashSync("tester", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Alex",
+    email: "alex@gmail.com",
+    password: bcrypt.hashSync("alex123", 10),
+  },
+];
+
+export default users;
