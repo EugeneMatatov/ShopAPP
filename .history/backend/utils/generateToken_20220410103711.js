6759586de05454c26728e1c58965fc0309dd5620
Eugene <eugene.matatov@gmail.com>
2022-04-12 20:56:03 +0300

    backupV4

---
+++ .history/backend/utils/generateToken_20220410103711.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { // (Synchronous) Returns the JsonWebToken as string
+    expiresIn: '30d',
+  })
+}
+
+export default generateToken
